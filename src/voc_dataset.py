0ff6f0f7cdc5fd8acd7d3d20e4d8ee1e8b782b4a
phanthaithuc <phanthai@arcada.fi>
2019-04-20 14:12:29 +0300

    fix

--- src/voc_dataset.py
+++ src/voc_dataset.py
@@ -1,3 +1,6 @@
+"""
+@author: Viet Nguyen <nhviet1009@gmail.com>
+"""
 import os
 import torch
 import torch.nn as nn
@@ -29,18 +32,19 @@ class VOCDataset(Dataset):
                 id_list_path = os.path.join(self.data_path, "list/train_aug.txt")
             else:
                 id_list_path = os.path.join(self.data_path, "list/val.txt")
-        
+
         self.ids = [id.strip() for id in open(id_list_path)]
-        self.classes = ['aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car', 'cat', 'chair', 'cow','diningtable', 'dog', 'horse', 'motorbike', 'person', 'pottedplant', 'sheep', 'sofa', 'train',
+        self.classes = ['aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car', 'cat', 'chair', 'cow',
+                        'diningtable', 'dog', 'horse', 'motorbike', 'person', 'pottedplant', 'sheep', 'sofa', 'train',
                         'tvmonitor']
         self.image_size = image_size
         self.num_classes = len(self.classes)
         self.num_images = len(self.ids)
         self.is_training = is_training
 
-def __len__(self):
-    return self.num_images
-    
+    def __len__(self):
+        return self.num_images
+
     def __getitem__(self, item):
         id = self.ids[item]
         if self.dataset in ["voc2007", "voc2012"]:
@@ -53,22 +57,22 @@ def __len__(self):
         image[:, :, 0] -= 104.008
         image[:, :, 1] -= 116.669
         image[:, :, 2] -= 122.675
-        
+
         gt_image = Image.open(gt_image_path).convert('P')
         gt_image = np.asarray(gt_image, np.int32)
         gt_image[gt_image == 255] = 0
-        
+
         image = cv2.resize(image, (self.image_size, self.image_size), interpolation=cv2.INTER_LINEAR)
         gt_image = cv2.resize(gt_image, (self.image_size, self.image_size), interpolation=cv2.INTER_NEAREST)
         gt_torch = torch.Tensor(torch.from_numpy(gt_image[None, None, :, :]).float())
-        
+
         gt1_size = ceil(self.image_size / 8.)
         interp = nn.Upsample(size=(gt1_size, gt1_size), mode='bilinear', align_corners=True)
         gt1 = interp(gt_torch).data.numpy()[0, 0, :, :]
-        
+
         gt2_size = ceil(self.image_size / 16.)
         interp = nn.Upsample(size=(gt2_size, gt2_size), mode='bilinear', align_corners=True)
         gt2 = interp(gt_torch).data.numpy()[0, 0, :, :]
-        
+
         return np.transpose(np.array(image, dtype=np.float32), (2, 0, 1)), np.array(gt1, dtype=np.float32), np.array(
-                                                                                                                     gt2, dtype=np.float32)
+            gt2, dtype=np.float32)
